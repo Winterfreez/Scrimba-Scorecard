54ab588c47edc850a097a32916cc3df7685bf5ae
Winterfreez <angeltechservices@gmail.com>
2022-11-03 13:50:58 -0400

    Styled code
    No content changes, just ran prettier plugin to style code.

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-let homeScore = 0
-let guestScore = 0
-let homeEl = document.getElementById("homeScore")
-let guestEl = document.getElementById("guestScore")
+let homeScore = 0;
+let guestScore = 0;
+let homeEl = document.getElementById("homeScore");
+let guestEl = document.getElementById("guestScore");
 
 // Home Functions
 function homeAdd1() {
-    homeScore += 1
-    homeEl.textContent = homeScore
+  homeScore += 1;
+  homeEl.textContent = homeScore;
 }
 
 function homeAdd2() {
-    homeScore += 2
-    homeEl.textContent = homeScore
+  homeScore += 2;
+  homeEl.textContent = homeScore;
 }
 
 function homeAdd3() {
-    homeScore += 3
-    homeEl.textContent = homeScore
+  homeScore += 3;
+  homeEl.textContent = homeScore;
 }
 
 // Guest Functions
 function guestAdd1() {
-    guestScore += 1
-    guestEl.textContent = guestScore
+  guestScore += 1;
+  guestEl.textContent = guestScore;
 }
 
 function guestAdd2() {
-    guestScore += 2
-    guestEl.textContent = guestScore
+  guestScore += 2;
+  guestEl.textContent = guestScore;
 }
 
 function guestAdd3() {
-    guestScore += 3
-    guestEl.textContent = guestScore
+  guestScore += 3;
+  guestEl.textContent = guestScore;
 }
 
 // Reset Function
 function reset() {
-    homeScore = 0
-    guestScore = 0
-    homeEl.textContent = homeScore
-    guestEl.textContent = guestScore
+  homeScore = 0;
+  guestScore = 0;
+  homeEl.textContent = homeScore;
+  guestEl.textContent = guestScore;
 }
